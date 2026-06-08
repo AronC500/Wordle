939c4d231a1427d46626125797682f621ad7c102
AronC500 <aronchen500@gmail.com>
2026-06-07 23:18:15 -0400

    added keyboard support, animation, and color for three cases

--- wordle-app/src/App.jsx
+++ wordle-app/src/App.jsx
@@ -1,7 +1,8 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
+    const currentword = 'LOVER'
     const [guesses, setGuesses] = useState([
         ['', '', '', '', ''],
         ['', '', '', '', ''],
@@ -18,21 +19,67 @@ function App() {
         ['ENTER','Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DELETE']
     ]
 
-    function keyPress(letter) {
-        if (currentCol === 5 || currentRow === 6) {
-            return;
+    function handleKeyDown(e) {
+        if (e.key === 'Enter') {
+            keyPress('ENTER')
         }
-        if (letter === 'ENTER') {
-            setCurrentRow(currentRow++)
+        else if (e.key === 'Backspace') {
+            keyPress('DELETE')
+        }
+        else if (e.key.match(/^[a-zA-Z]$/)) {  
+            keyPress(e.key.toUpperCase())
+        }
+        return;
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown)
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown)
+        }
+    }, [currentCol, currentRow])
+
+    function determineColor(letter, colIndex, rowIndex) {
+        if (letter.length === 0) {
+            return "letter"
+        }
+        if (rowIndex === currentRow) {
+            return "letterwithborder"
+        }
+        if (letter === currentword[colIndex]) {
+            return "green"
+        }
+        if (currentword.includes(letter)) {
+            return "yellow"
+        }
+        else {
+            return "gray"
+        }
+    }
+    function keyPress(letter) {
+        if (currentCol === 5) {
+            if (letter === 'ENTER' && currentRow !== 6) {
+                setCurrentRow(currentRow + 1)
+                setCurrentCol(0)
+                return
+            }
+            if (letter !== 'DELETE') {
+                return;
+            }
+            
         }
         const newArray = guesses.map((row) => [...row])
-
-        if (letter === 'DELETE' && currentCol !== 0) {
+        if (letter === 'DELETE') {
+            if (currentCol === 0) {
+                return;
+            }
             newArray[currentRow][currentCol - 1] = ''
             setCurrentCol(currentCol - 1)
             setGuesses(newArray)
             return;
         }
+
+
         newArray[currentRow][currentCol] = letter
         setGuesses(newArray)
         setCurrentCol(currentCol + 1)
@@ -44,7 +91,7 @@ function App() {
         {guesses.map((row, rowIndex) => (
             <div key={rowIndex} className="word"> 
             {row.map((letter, colIndex) => (
-                <div key={colIndex} className="letter">
+                <div key={letter === '' ? `${rowIndex}${colIndex}` :`${rowIndex}${colIndex}${letter}`} className={determineColor(letter, colIndex, rowIndex)}>
                     {letter}
                 </div>
             ))}
@@ -57,7 +104,7 @@ function App() {
             {row.map((letter,colIndex) => (
                 <button onClick = {() => keyPress(letter)} key={colIndex} className={letter === "ENTER" ? "ENTER" : letter === "DELETE" ? "DELETE" : "keyboardletter"}>
                     {letter}
-                 </button>
+                </button>
             ))}
             </div>
         ))}
