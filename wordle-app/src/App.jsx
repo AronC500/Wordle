58a7e5810b7945bd3888d4e725542d40e3409385
AronC500 <aronchen500@gmail.com>
2026-06-16 21:36:20 -0400

    added jump, shake, not enough letter or not valid word animation and keyboard col or change

--- wordle-app/src/App.jsx
+++ wordle-app/src/App.jsx
@@ -1,12 +1,16 @@
 import {useState, useEffect} from 'react'
 import './App.css'
+import { validWords, answerList } from './wordlist.js'
+import {useNavigate} from 'react-router-dom'
 
 function App() {
-    
-    const currentword = 'LOVER'
+    const startDate = new Date(2021,5,19)
+    const today = new Date()
+    const daysDifference = Math.floor((today-startDate) / (1000*60*60*24))
+    const currentWord = answerList[daysDifference % answerList.length]
+    const [shakeRow, setShakeRow] = useState(null)
+    const navigate = useNavigate()
     const [showBurger, setShowBurger] = useState(false)
-    const [animationStyle, setanimationStlye] = useState('')
-
     const [guesses, setGuesses] = useState([
         ['', '', '', '', ''],
         ['', '', '', '', ''],
@@ -17,6 +21,12 @@ function App() {
     ])    
     const [currentRow, setCurrentRow] = useState(0)
     const [currentCol, setCurrentCol] = useState(0)
+    const [disableInput, setdisableInput] = useState(false)
+    const [showGamePopup, setShowGamePopup] = useState(false)
+    const [MessagetoShow, setMessagetoShow] = useState('')
+    const [gameOver, isGameOver] = useState(false)
+    const [winRow, setwinRow] = useState(null)
+    const [keyboardColor, setkeyboardColor] = useState({})
     const keyboard = [
         ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
         ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
@@ -33,15 +43,16 @@ function App() {
         else if (e.key.match(/^[a-zA-Z]$/)) {  
             keyPress(e.key.toUpperCase())
         }
-        return;
+        return
     }
-
     useEffect(() => {
-        window.addEventListener('keydown', handleKeyDown)
+        if (!disableInput) {
+            window.addEventListener('keydown', handleKeyDown)
+        }        
         return () => {
             window.removeEventListener('keydown', handleKeyDown)
         }
-    }, [currentCol, currentRow])
+    }, [currentCol, currentRow, disableInput])
 
     function determineColor(letter, colIndex, rowIndex) {
         if (letter.length === 0) {
@@ -50,10 +61,10 @@ function App() {
         if (rowIndex === currentRow) {
             return "letterwithborder"
         }
-        if (letter === currentword[colIndex]) {
+        if (letter.toLowerCase() === currentWord[colIndex]) {
             return "green"
         }
-        if (currentword.includes(letter)) {
+        if (currentWord.includes(letter.toLowerCase())) {
             return "yellow"
         }
         else {
@@ -61,16 +72,98 @@ function App() {
         }
     }
     function keyPress(letter) {
+        if (disableInput) {
+            return
+        }
         if (currentCol === 5) {
             if (letter === 'ENTER' && currentRow !== 6) {
+                const word = guesses[currentRow].join('').toLowerCase();
+                if (!validWords.includes(word)) {
+                    setMessagetoShow("Not in word list")
+                    setShowGamePopup(true)
+                    setShakeRow(currentRow)
+                    setTimeout(() => {
+                        setShakeRow(null)
+                    }, 500)
+                    setTimeout(() => {
+                        setShowGamePopup(false)
+                    }, 2000)
+                    return
+                }
+                else if (word === currentWord) {
+                    if (currentRow === 0) {
+                        setMessagetoShow("Genius")
+                    }
+                    if (currentRow === 1) {
+                        setMessagetoShow("Magnificient")
+                    }
+                    if (currentRow === 2) {
+                        setMessagetoShow("Impressive")
+                    }
+                    if (currentRow === 3) {
+                        setMessagetoShow("Splendid")
+                    }
+                    if (currentRow === 4) {
+                        setMessagetoShow("Great")
+                    }
+                    if (currentRow === 5) {
+                        setMessagetoShow("Phew")
+                    }
+                    setTimeout(() => {
+                        setwinRow(currentRow)
+                    },1800)
+                    setdisableInput(true)
+                    setTimeout(() => {
+                        isGameOver(true)
+                    },3600)
+                    setTimeout(()=> {
+                        setShowGamePopup(true)
+                    },1800)
+                    setTimeout(() => {
+                        setShowGamePopup(false)
+                    }, 5000)
+                }
+                else {
+                    setTimeout(() => {
+                        setdisableInput(false)
+                        word.split('').forEach((letter, colIndex) => {
+                            setkeyboardColor(prev => {
+                                const newobj = {...prev}
+                                if (letter.toLowerCase() === currentWord[colIndex]) {
+                                    newobj[letter.toLowerCase()] = 'green'
+                                } else if (currentWord.includes(letter.toLowerCase())) {
+                                    newobj[letter.toLowerCase()] = 'yellow'
+                                } else {
+                                    newobj[letter.toLowerCase()] = 'gray'
+                                }
+                                return newobj
+                            })
+                        })
+                    },1800)
+                    setdisableInput(true)
+                }
                 setCurrentRow(currentRow + 1)
                 setCurrentCol(0)
-                return
+                
+                
             }
             if (letter !== 'DELETE') {
                 return;
             }
+        }
             
+
+        if (letter === 'ENTER' && currentCol !== 5) {
+            setMessagetoShow("Not enough letters")
+            setShowGamePopup(true)
+            setShakeRow(currentRow)
+            setTimeout(() => {
+                setShakeRow(null)
+            }, 500)
+            setTimeout(() => {
+                setShowGamePopup(false)
+            }, 2000)
+            return;
         }
         const newArray = guesses.map((row) => [...row])
         if (letter === 'DELETE') {
@@ -83,7 +176,7 @@ function App() {
             return;
         }
 
-
+//also understand the useeffect
         newArray[currentRow][currentCol] = letter
         setGuesses(newArray)
         setCurrentCol(currentCol + 1)
@@ -100,27 +193,41 @@ function App() {
 
       </div>
       <div className="board">
+        {showGamePopup &&
+        <div className={"popupmessage"}>{MessagetoShow}</div>
+        
+        }
         {guesses.map((row, rowIndex) => (
-            <div key={rowIndex} className="word"> 
+            <div key={rowIndex} className={shakeRow === rowIndex ? "wordanimation" : "word"}> 
             {row.map((letter, colIndex) => (
-                <div key={letter === '' ? `${rowIndex}${colIndex}` :`${rowIndex}${colIndex}${letter}`} className={determineColor(letter, colIndex, rowIndex)} style={{ animationDelay: `${colIndex * 0.3}s` }}>
+                <div key={letter === '' ? `${rowIndex}${colIndex}` :`${rowIndex}${colIndex}${letter}`} className={winRow === rowIndex ? 'jump' : determineColor(letter, colIndex, rowIndex)} style={{ animationDelay: `${colIndex * 0.3}s` }}>
                     {letter}
                 </div>
             ))}
             </div>
         ))}
       </div>
-      <div className="keyboard">
+      {!gameOver && 
+        <div className="keyboard">
         {keyboard.map((row,rowIndex) => (
             <div key={rowIndex} className="keyboardrow"> 
             {row.map((letter,colIndex) => (
-                <button onClick = {() => keyPress(letter)} key={colIndex} className={letter === "DELETE" ? "DELETE" : letter === "ENTER" ? "ENTER" : "keyboardletter"}>
+                <button onClick = {() => keyPress(letter)} key={colIndex} className={keyboardColor[letter.toLowerCase()] === 'green' ? 'keyboardgreen' : keyboardColor[letter.toLowerCase()] === 'yellow' ? 'keyboardyellow' : keyboardColor[letter.toLowerCase()] === 'gray' ? 'keyboardgray' : letter === "DELETE" ? "DELETE" : letter === "ENTER" ? "ENTER" : "keyboardletter"
+            }>
                     {letter}
                 </button>
             ))}
             </div>
         ))}
       </div> 
+      }
+      {gameOver &&
+        <div className="gameoverbuttons">
+            <button className="resultbutton">See Results</button>
+            <button onClick={()=> navigate('/')}className="homebutton">Home</button>
+        </div>
+      }
+      
       <div className={showBurger ? "overlay" : "overlayHidden"}>
         <div className="overlayheader">
             <button className="closebutton" onClick={() => setShowBurger(false)}>
