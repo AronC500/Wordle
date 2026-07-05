277a6d77ccda3fdc9498f10510414c23e23909cc
AronC500 <aronchen500@gmail.com>
2026-07-05 12:39:40 -0400

    G

--- wordle-app/server/server.js
+++ wordle-app/server/server.js
@@ -12,13 +12,12 @@ const resend = new Resend(process.env.RESEND)
 const app = express()
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-
-
-})
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
+});
 db.connect((err)=> {
     if (err) {
         console.log(err)
