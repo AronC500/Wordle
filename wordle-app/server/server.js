e1cbe275a4f40215b8b9d037c5b38338b17b3ede
AronC500 <aronchen500@gmail.com>
2026-07-04 22:56:23 -0400

    add SSL and port config for TiDB connection

--- wordle-app/server/server.js
+++ wordle-app/server/server.js
@@ -14,7 +14,12 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
 })
 
 db.connect((err)=> {
